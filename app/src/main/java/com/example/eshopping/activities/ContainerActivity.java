b5e690c28c17f70473d5308ede76be58f6b4f120
Shimul <sajdulshimul@gmail.com>
2023-02-09 20:49:17 +0600

    Bottom navigation coding

--- app/src/main/java/com/example/eshopping/activities/ContainerActivity.java
+++ app/src/main/java/com/example/eshopping/activities/ContainerActivity.java
@@ -2,15 +2,50 @@ package com.example.eshopping.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.eshopping.R;
+import com.example.eshopping.fragments.CartFragment;
+import com.example.eshopping.fragments.LoveProductFragment;
+import com.example.eshopping.fragments.ProfileFragment;
 
 public class ContainerActivity extends AppCompatActivity {
+
+    LoveProductFragment loveProductFragment = new LoveProductFragment();
+    CartFragment cartFragment = new CartFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+    Intent intent;
+    String loveMenu, cartMenu, profileMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
+
+        intent = getIntent();
+        loveMenu = intent.getStringExtra("loveMenu");
+        cartMenu = intent.getStringExtra("cartMenu");
+        profileMenu = intent.getStringExtra("profileMenu");
+
+        if (loveMenu != null){
+            if (loveMenu.equals("loveMenu")){
+                getSupportFragmentManager().beginTransaction().replace(R.id.containerActivity, loveProductFragment).commit();
+            }
+        }
+
+        if (cartMenu != null){
+            if (cartMenu.equals("cartMenu")){
+                getSupportFragmentManager().beginTransaction().replace(R.id.containerActivity, cartFragment).commit();
+            }
+        }
+
+        if (profileMenu != null){
+            if (profileMenu.equals("profileMenu")){
+                getSupportFragmentManager().beginTransaction().replace(R.id.containerActivity, profileFragment).commit();
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/eshopping/activities/MainActivity.java
+++ app/src/main/java/com/example/eshopping/activities/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.eshopping.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.eshopping.R;
 import com.example.eshopping.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +22,39 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.bottomMenu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
+                switch (item.getItemId()){
+
+                    case R.id.homeMenu:
+                        Toast.makeText(MainActivity.this, "Home Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.loveMenu:
+                        Intent intent1 = new Intent(getApplicationContext(), ContainerActivity.class);
+                        intent1.putExtra("loveMenu", "loveMenu");
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.cartMenu:
+                        Intent intent2 = new Intent(getApplicationContext(), ContainerActivity.class);
+                        intent2.putExtra("cartMenu", "cartMenu");
+                        startActivity(intent2);
+                        break;
+
+                    case R.id.profileMenu:
+                        Intent intent3 = new Intent(getApplicationContext(), ContainerActivity.class);
+                        intent3.putExtra("profileMenu", "profileMenu");
+                        startActivity(intent3);
+                        break;
+
+                }
+
+                return false;
+            }
+        });
 
     }
 }
